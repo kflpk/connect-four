deff7fec5d9ac753a30b213abbda615a78851197
Yoshua-chan <31251884+Yoshua-chan@users.noreply.github.com>
2022-05-19 20:01:18 +0200

    minor reformats in extras.cpp

--- src/extras.cpp
+++ src/extras.cpp
@@ -16,16 +16,16 @@ void wfill(WINDOW* win, char c) {
 }
 
 void init_theme() {
-    init_color(COLOR_ORANGE, 900, 600, 0);
-    init_color(COLOR_FRAME, 750, 750, 750);
+    init_color(COLOR_ORANGE,     900, 600,   0);
+    init_color(COLOR_FRAME,      750, 750, 750);
     init_color(COLOR_BACKGROUND, 150, 150, 150);
 
-    init_pair(PAIR_BLUE, COLOR_BLUE, COLOR_BACKGROUND);
-    init_pair(PAIR_ORANGE, COLOR_ORANGE, COLOR_BACKGROUND);
-    init_pair(PAIR_FRAME, COLOR_FRAME, COLOR_FRAME);
-    init_pair(PAIR_DEFAULT, COLOR_WHITE, COLOR_BACKGROUND);
+    init_pair(PAIR_BLUE,    COLOR_BLUE,   COLOR_BACKGROUND);
+    init_pair(PAIR_ORANGE,  COLOR_ORANGE, COLOR_BACKGROUND);
+    init_pair(PAIR_FRAME,   COLOR_FRAME,  COLOR_FRAME);
+    init_pair(PAIR_DEFAULT, COLOR_WHITE,  COLOR_BACKGROUND);
     init_pair(PAIR_PLAYER1, COLOR_ORANGE, COLOR_BLUE);
-    init_pair(PAIR_PLAYER2, COLOR_BLUE, COLOR_ORANGE);
+    init_pair(PAIR_PLAYER2, COLOR_BLUE,   COLOR_ORANGE);
 
     curs_set(0);
     //init_pair(PAIR_PLAYER1, COLOR_ORANGE)
